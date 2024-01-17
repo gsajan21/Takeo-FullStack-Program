2bbe474f80add325a661389fb0768da6e043dcb3
Sajan Gurung <gsajan21@gmail.com>
2024-01-17 09:05:07 -0800

    commit test

---
+++ Day2/src/main/java/org/example/DataTypes.java
@@ -0,0 +1,27 @@
+package org.example;
+public class DataTypes {
+
+    int number = 10;
+    boolean bool = false;
+
+    float fNumber= 0.002f;
+
+    char someChar = 'a';
+
+    String someString = "You can write anything here.";
+
+    void printDataTypes(){
+        System.out.println("This is a int type: "+number);
+        System.out.println("This is a boolean type: "+bool);
+        System.out.println("This is a float type: "+fNumber);
+        System.out.println("This is a char type: "+someChar);
+        System.out.println("This is a String type: "+someString);
+    }
+
+    public static void main(String[] args) {
+        DataTypes dataTypes = new DataTypes();
+
+        dataTypes.printDataTypes();
+
+    }
+}
---
+++ Day2/src/main/java/org/example/Sample.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Sample {
+
+
+
+    void test1() {
+
+        float z =	test(10,23.5f);
+
+        System.out.println(z);
+    }
+
+    float test( int a,float b) {
+
+
+
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+
+        Sample s = new Sample();
+        s.test1();
+
+    }
+
+}
